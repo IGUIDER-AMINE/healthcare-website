23f34e0c672130ef3c0b1fbbaf639fb901dc6ded
IGUIDER-AMINE <amineiguider2017@gmail.com>
2023-06-11 14:02:59 +0200

    responsive desgin

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import Link from "next/link";
+import { SlSocialYoutube } from "react-icons/sl";
+import {
+  SlSocialLinkedin,
+  SlSocialFacebook,
+  SlSocialInstagram,
+} from "react-icons/sl";
 
 const Footer = () => {
   return (
     <section id="footer" className="py-7 px-4 bg-bodyColor">
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 container leading-10 mx-auto text-gray-400 text-[15px]">
+      <div className="grid uppercase grid-cols-1 md:grid-cols-4 lg:grid-cols-5 container leading-10 mx-auto text-gray-400 text-[15px]">
         <div className="flex flex-col justify-between">
           <div>
             <a href="#">🌃Pharmadi</a>
           </div>
           <div>Pharmadi inc, 2023</div>
         </div>
-        <div className="uppercase">
+        <div>
           <div>
             <a href="#">Contact Us</a>
           </div>
@@ -21,7 +27,7 @@ const Footer = () => {
             <a href="#">Terms of use</a>
           </div>
         </div>
-        <div className="uppercase">
+        <div>
           <div>
             <a href="#">About Us</a>
           </div>
@@ -32,7 +38,7 @@ const Footer = () => {
             <a href="#">BLOG</a>
           </div>
         </div>
-        <div className="uppercase">
+        <div>
           <div>
             <a href="#">Follow Us</a>
           </div>
@@ -40,12 +46,31 @@ const Footer = () => {
             <a href="#">Disclaimer</a>
           </div>
         </div>
-        <div className="uppercase flex flex-col justify-between">
+        <div className="flex flex-col justify-between">
           <div>
             <a href="#">(677) 234-4566</a>
           </div>
-          <div>
-            <a href="#">Disclaimer</a>
+          <div className="flex gap-3">
+            <Link href="#footer">
+              <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+                <SlSocialFacebook />
+              </span>
+            </Link>
+            <Link href="#footer">
+              <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+                <SlSocialYoutube />
+              </span>
+            </Link>
+            <Link href="#footer">
+              <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+                <SlSocialInstagram />
+              </span>
+            </Link>
+            <Link href="#footer">
+              <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+                <SlSocialLinkedin />
+              </span>
+            </Link>
           </div>
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,13 +21,13 @@ export default function Home() {
       <main className="w-full overflow-hidden flex flex-col gap-24 justify-between text-[16px] font-bodyFont bg-white text-textLight">
         {/* <main className="w-full container mx-auto text-[16px] h-screen font-bodyFont bg-white text-textLight overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60"> */}
         {/* <NavBar /> */}
-        <Banner />
+        {/* <Banner />
         <OurFeatures />
         <OurDoctors />
         <Care />
         <ClientQsts />
         <Testimonials />
-        <OurApp />
+        <OurApp /> */}
         <Footer />
       </main>
     </>
